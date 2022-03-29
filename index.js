416b8635f4aecea09e039fb931156323f3e0f16c
Carter Kearns <coder.kearns@gmail.com>
2022-03-29 16:25:37 -0500

    Move vars.js to single file and remove dependancies to allow client-side use

--- index.js
+++ index.js
@@ -1,5 +1,60 @@
-const vars = require('./vars');
-const axios = require('axios');
+const vars = {
+    // Urls:
+    dexcomBaseUrl: "https://share2.dexcom.com/ShareWebServices/Services",
+    dexcomBaseUrlOus: "https://shareous1.dexcom.com/ShareWebServices/Services",
+    // Endpoints:
+    dexcomLoginEndpoint: "/General/LoginPublisherAccountByName",
+    dexcomAuthenticateEndpoint: "/General/AuthenticatePublisherAccount",
+    dexcomVerifySerialNumberEndpoint:
+        "/Publisher/CheckMonitoredReceiverAssignmentStatus",
+    dexcomGlucoseReadingsEndpoint: "/Publisher/ReadPublisherLatestGlucoseValues",
+    // Application ID:
+    dexcomApplicationId: "d89443d2-327c-4a6f-89e5-496bbb0317db",
+    // Error messages:
+    accountErrorUsernameNullEmpty: "Username null or empty",
+    accountErrorPasswordNullEmpty: "Password null or empty",
+    accountErrorAccountNotFound: "Account not found",
+    accountErrorPasswordInvalid: "Password not valid",
+    accountErrorUnknown: "Account error",
+    sessionErrorSessionIdNull: "Session ID null",
+    sessionErrorSessionIdDefault: "Session ID default",
+    sessionErrorSessionNotValid: "Session ID not valid",
+    sessionErrorSessionNotFound: "Session ID not found",
+    arguementErrorMinutesInvalid: "Minutes must be between 1 and 1440",
+    arguementErrorMaxCountInvalid: "Max count must be between 1 and 288",
+    arguementErrorSerialNumberNullEmpty: "Serial number null or empty",
+    // Trend Keys:
+    dexcomTrendKeys: {
+        "None": 0,
+        "DoubleUp": 1,
+        "SingleUp": 2,
+        "FortyFiveUp": 3,
+        "Flat": 4,
+        "FortyFiveDown": 5,
+        "SingleDown": 6,
+        "DoubleDown": 7,
+        "NotComputable": 8,
+        "RateOutOfRange": 9,
+    },
+    // Trend descriptions:
+    dexcomTrendDescriptions: [
+        "",
+        "rising quickly",
+        "rising",
+        "rising slightly",
+        "steady",
+        "falling slightly",
+        "falling",
+        "falling quickly",
+        "unable to determine trend",
+        "trend unavailable",
+    ],
+    dexcomTrendArrows: ["", "↑↑", "↑", "↗", "→", "↘", "↓", "↓↓", "?", "-"],
+    // Default session ID:
+    defaultSessionId: "00000000-0000-0000-0000-000000000000",
+    // Conversion factors:
+    mmolLConvertionFactor: 0.0555, // (mmol/L) = (mg/dl) * 0.0555
+}
 
 class GlucoseReading {
     constructor(reading) {
@@ -32,6 +87,8 @@ class GlucoseReading {
 }
 
 class Dexcom {
+    static Reading = GlucoseReading;
+
     constructor(username, password, ous=false) {
         if (!username || !password) {
             throw new Error('Username and password are required');
@@ -130,4 +187,7 @@ class Dexcom {
 
 }
 
-module.exports = Dexcom;
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    // Export the Dexcom class when used as a node module
+    module.exports = Dexcom;
+}
